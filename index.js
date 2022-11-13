913726d28a4a073b15925385ae103320377a945e
jheave <jimenezjheave123@gmail.com>
2022-11-13 18:15:39 +0800

    init

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
-``
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
